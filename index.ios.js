c1147d68a2f034bf8c1b8e1e38ad3e1b4dd6c92f
Michael Anderson <normanganderson@gmail.com>
2016-02-09 22:38:04 -0700

    Added some linting and the beginnings of a webview for Reddit oauth

--- index.ios.js
+++ index.ios.js
@@ -2,51 +2,70 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-'use strict';
 import React, {
   AppRegistry,
-  Component,
   StyleSheet,
   Text,
   View
+  // WebView
 } from 'react-native';
 
-class Reddit extends Component {
+// const WEBVIEW_REF = 'webview';
+// const renderRedditAuth = ({state}) => {
+//   return (
+//     <WebView
+//       automaticallyAdjustContentInsets={false}
+//       decelerationRate="normal"
+//       domStorageEnabled={true}
+//       javaScriptEnabled={true}
+//       // onNavigationStateChange={this.onNavigationStateChange}
+//       // onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest}
+//       ref={WEBVIEW_REF}
+//       scalesPageToFit={this.state.scalesPageToFit}
+//       source={{uri: state.url}}
+//       startInLoadingState={true}
+//       style={styles.webView}
+//     />
+//   );
+// };
+
+var Reddit = React.createClass({
+  displayName: 'Reddit',
+  getInitialState() {
+    return {
+      url: 'https://ssl.reddit.com/api/v1/authorize?client_id=t7x3gcvSp8A0_A&response_type=code&state=TEST&redirect_uri=reactNativeReddit://response&duration=permanent&scope=read'
+    };
+  },
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          I'm dying to do something good!
         </Text>
       </View>
+        // {renderRedditAuth(this.state)}
     );
   }
-}
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
+  // welcome: {
+  //   fontSize: 20,
+  //   textAlign: 'center',
+  //   margin: 10
+  // },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
 
 AppRegistry.registerComponent('Reddit', () => Reddit);
